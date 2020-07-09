e27891767bd3d8993e6cc55be0757a0922366c97
YukoSky <daffetyxd@gmail.com>
2020-07-09 12:46:04 -0300

    roms type: added pixel, rog & zui

---
+++ Pixel/build.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+# Sync
+telegram -M "Pixel: Build started"
+SYNC_START=$(date +"%s")
+
+sudo ./ErfanGSIs/url2GSI.sh $ROM_LINK Pixel
+    mkdir final
+
+    SYNC_END=$(date +"%s")
+    SYNC_DIFF=$((SYNC_END - SYNC_START))
+    telegram -M "Pixel: Build completed successfully in $((SYNC_DIFF / 60)) minute(s) and $((SYNC_DIFF % 60)) seconds"
+
+    SYNC_START=$(date +"%s")
+    telegram -M "Pixel: Zipping output started"
+
+    export date2=`date +%Y%m%d%H%M`
+    export sourcever2=`cat ./ErfanGSIs/ver`
+    sudo chmod -R 777 ErfanGSIs/output
+               
+    cd ErfanGSIs/output/
+               
+    curl -sL https://git.io/file-transfer | sh
+               
+    zip -r $ROM-AB-$sourcever2-$date2-ErfanGSI-YuMiGSI.7z *-AB-*.img
+    zip -r $ROM-Aonly-$sourcever2-$date2-ErfanGSI-YuMiGSI.7z *-Aonly-*.img
+
+    SYNC_END=$(date +"%s")
+    SYNC_DIFF=$((SYNC_END - SYNC_START))
+    telegram -M "Pixel: Zipping completed successfully in $((SYNC_DIFF / 60)) minute(s) and $((SYNC_DIFF % 60)) seconds"
+
+    SYNC_START=$(date +"%s")
+    telegram -M "Pixel: Upload started"
+
+    ./transfer $MIR $ROM-Aonly-$sourcever2-$date2-ErfanGSI-YuMiGSI.7z
+    ./transfer $MIR $ROM-AB-$sourcever2-$date2-ErfanGSI-YuMiGSI.7z
+
+    SYNC_END=$(date +"%s")
+    SYNC_DIFF=$((SYNC_END - SYNC_START))
+    telegram -M "Pixel: Uploading completed successfully in $((SYNC_DIFF / 60)) minute(s) and $((SYNC_DIFF % 60)) seconds"
