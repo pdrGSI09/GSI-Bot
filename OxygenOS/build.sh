0c646dbe9cbc937bd40bc2b49ce53cc47b7ee6c0
YukoSky <daffetyxd@gmail.com>
2020-07-10 17:12:53 -0300

    fixing issues

--- OxygenOS/build.sh
+++ OxygenOS/build.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 # Sync
-telegram -M "JoyUI: Build started"
+telegram -M "OxygenOS: Build started"
 SYNC_START=$(date +"%s")
 
-sudo ./ErfanGSIs/url2GSI.sh $ROM_LINK JoyUI
+sudo ./ErfanGSIs/url2GSI.sh $ROM_LINK OxygenOS
     mkdir final
 
     SYNC_END=$(date +"%s")
     SYNC_DIFF=$((SYNC_END - SYNC_START))
-    telegram -M "JoyUI: Build completed successfully in $((SYNC_DIFF / 60)) minute(s) and $((SYNC_DIFF % 60)) seconds"
+    telegram -M "OxygenOS: Build completed successfully in $((SYNC_DIFF / 60)) minute(s) and $((SYNC_DIFF % 60)) seconds"
 
     SYNC_START=$(date +"%s")
-    telegram -M "JoyUI: Zipping output started"
+    telegram -M "OxygenOS: Zipping output started"
 
     export date2=`date +%Y%m%d%H%M`
     export sourcever2=`cat ./ErfanGSIs/ver`
@@ -26,14 +26,14 @@ sudo ./ErfanGSIs/url2GSI.sh $ROM_LINK JoyUI
 
     SYNC_END=$(date +"%s")
     SYNC_DIFF=$((SYNC_END - SYNC_START))
-    telegram -M "JoyUI: Zipping completed successfully in $((SYNC_DIFF / 60)) minute(s) and $((SYNC_DIFF % 60)) seconds"
+    telegram -M "OxygenOS: Zipping completed successfully in $((SYNC_DIFF / 60)) minute(s) and $((SYNC_DIFF % 60)) seconds"
 
     SYNC_START=$(date +"%s")
-    telegram -M "JoyUI: Upload started"
+    telegram -M "OxygenOS: Upload started"
 
     echo "::set-env name=DOWNLOAD_A::$(./transfer $MIR "$ROM-Aonly-$sourcever2-$date2-ErfanGSI-YuMiGSI.7z" | grep -o -P '(?<=Download Link: )\S+')"
     echo "::set-env name=DOWNLOAD_AB::$(./transfer $MIR "$ROM-AB-$sourcever2-$date2-ErfanGSI-YuMiGSI.7z" | grep -o -P '(?<=Download Link: )\S+')"
 
     SYNC_END=$(date +"%s")
     SYNC_DIFF=$((SYNC_END - SYNC_START))
-    telegram -M "JoyUI: Uploading completed successfully in $((SYNC_DIFF / 60)) minute(s) and $((SYNC_DIFF % 60)) seconds"
+    telegram -M "OxygenOS: Uploading completed successfully in $((SYNC_DIFF / 60)) minute(s) and $((SYNC_DIFF % 60)) seconds"
