a0ef4b69898e00d0afe8623c726787a6a4fb4d70
YukoSky <daffetyxd@gmail.com>
2020-07-11 12:02:53 -0300

    workflow: try to fix error

--- JoyUI/initial.sh
+++ JoyUI/initial.sh
@@ -4,8 +4,9 @@ telegram -M "JoyUI: Sync started"
 SYNC_START=$(date +"%s")
 
 sudo -E apt-get -qq update
-sudo -E apt-get -qq install git openjdk-8-jdk wget expect
+sudo -E apt-get -qq install git openjdk-8-jdk wget expect yarn
 pip install pyrogram tgcrypto
+yarn build
 
 SYNC_END=$(date +"%s")
 SYNC_DIFF=$((SYNC_END - SYNC_START))
