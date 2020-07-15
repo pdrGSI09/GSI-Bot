d48c16da2c7105643c7c6390f802cd33ba8bcacd
henloboi <henloboii@protonmail.ch>
2020-07-15 18:30:52 +0800

    ffs stfu

--- init.sh
+++ init.sh
@@ -1,12 +1,7 @@
 #!/bin/bash
 # Sync
-telegram -M "$ROM: Configure Environment"
 SYNC_START=$(date +"%s")
 
 sudo -E apt-get -qq update
 sudo -E apt-get -qq install git openjdk-8-jdk wget expect
 pip install pyrogram tgcrypto
-
-SYNC_END=$(date +"%s")
-SYNC_DIFF=$((SYNC_END - SYNC_START))
-telegram -M "$ROM: Config done in  $((SYNC_DIFF / 60)) minute(s) and $((SYNC_DIFF % 60)) seconds"
