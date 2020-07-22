6e7ad26023c4871431f1966b0d1484ea8a84d3c0
ゆうこスカイ ~#YuMi1621 <daffetyxd@gmail.com>
2020-07-22 14:47:07 -0300

    (BUILD) Generic

--- .github/workflows/robot.py
+++ .github/workflows/robot.py
@@ -15,7 +15,7 @@ dab = os.getenv('DOWNLOAD_AB')
 
 with pyrogram.Client('bot', os.getenv('API_ID'), os.getenv('API_HASH'), bot_token=os.getenv('TOKEN')) as client:
     client.send_message(
-        text=f"""- <b>{rom} GSI For A-Only • A/B Devices (AICP)</b>
+        text=f"""- <b>{rom} GSI For A-Only • A/B Devices (AOSPA)</b>
 
 <b>- Firmware Base:</b> <a href="{romlink}">HERE</a>
 
