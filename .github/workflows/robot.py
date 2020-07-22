9654bdf1f4d79140df972fc3c5e6ab1cb0caaa94
ゆうこスカイ ~#YuMi1621 <daffetyxd@gmail.com>
2020-07-22 08:50:40 -0300

    (BUILD) Generic

--- .github/workflows/robot.py
+++ .github/workflows/robot.py
@@ -15,7 +15,7 @@ dab = os.getenv('DOWNLOAD_AB')
 
 with pyrogram.Client('bot', os.getenv('API_ID'), os.getenv('API_HASH'), bot_token=os.getenv('TOKEN')) as client:
     client.send_message(
-        text=f"""<b>{rom} GSI For A/AB Devices (/e/ ROM)</b>
+        text=f"""<b>{rom} GSI For A/AB Devices (CorvusOS v7.5)</b>
 
 <b>Firmware Base:</b> <a href="{romlink}">HERE</a>
 
