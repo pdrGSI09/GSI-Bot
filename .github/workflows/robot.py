7e4b3e55304596f2516c221a108471a8662b590c
YukoSky <daffetyxd@gmail.com>
2020-07-31 18:17:36 -0300

    (BUILD) Dirty Unicorn

--- .github/workflows/robot.py
+++ .github/workflows/robot.py
@@ -7,6 +7,7 @@ with open('Aonly.txt') as f:
     os.environ['CAT_FILE'] = f.read()
 
 rom = os.getenv('ROM')
+rom_generic = os.getenv('ROM_GENERIC')
 zip = os.getenv('ZIP_NAME')
 cat = os.getenv('CAT_FILE')
 romlink = os.getenv('ROM_LINK')
@@ -20,7 +21,7 @@ read = os.getenv('READ')
 
 with pyrogram.Client('bot', os.getenv('API_ID'), os.getenv('API_HASH'), bot_token=os.getenv('TOKEN')) as client:
     client.send_message(
-        text=f"""- <b>{rom} GSI For A-Only • A/B Devices</b>
+        text=f"""- <b>{rom} GSI For A-Only • A/B Devices ({rom_generic})</b>
 
 <b>- Firmware Base:</b> <a href="{romlink}">HERE</a>
 
--- Generic/build.sh
+++ Generic/build.sh
@@ -39,21 +39,6 @@ sudo ./ErfanGSIs/url2GSI.sh $ROM_LINK Generic:$ROM_GENERIC
     echo "::set-env name=WET_DOWNLOAD_A::$(./transfer wet "$ROM_GENERIC-Aonly-$sourcever2-$date2-ErfanGSI-YuMiGSI.zip" | grep -o -P '(?<=Download Link: )\S+')"
     echo "::set-env name=WET_DOWNLOAD_AB::$(./transfer wet "$ROM_GENERIC-AB-$sourcever2-$date2-ErfanGSI-YuMiGSI.zip" | grep -o -P '(?<=Download Link: )\S+')"
 
-    # SourceForge
-    export dateup2=`date +%Y%m%d`
-    echo "::set-env name=dateup::$(echo $dateup2)"
-
-    export TMPSFDIR=`cd /home/pfs/project/yumi-project/gsi/Generic/$ROM_GNERIC/$DEVICE/$dateup2`
-    echo "::set-env name=SFDIR::$(echo $TMPSFDIR)"
-
-    # Define temp var for SF_DOWNLOAD_A var
-    export TMPA=`https://sourceforge.net/projects/yumi-project/files/gsi/Generic/$ROM_GENERIC/$DEVICE/$ROM_GENERIC-Aonly-$sourcever2-$date2-ErfanGSI-YuMiGSI.zip/download`
-    echo "::set-env name=SF_DOWNLOAD_A::$(echo $TMPA)"
-
-    # Define temp var for SF_DOWNLOAD_AB var
-    export TMPAB=`https://sourceforge.net/projects/yumi-project/files/gsi/Generic/$ROM_GENERIC/$DEVICE/$ROM_GENERIC-AB-$sourcever2-$date2-ErfanGSI-YuMiGSI.zip/download`
-    echo "::set-env name=SF_DOWNLOAD_AB::$(echo $TMPAB)"
-
     SYNC_END=$(date +"%s")
     SYNC_DIFF=$((SYNC_END - SYNC_START))
     telegram -M "Generic ($ROM_GENERIC): Uploading completed successfully in $((SYNC_DIFF / 60)) minute(s) and $((SYNC_DIFF % 60)) seconds"
